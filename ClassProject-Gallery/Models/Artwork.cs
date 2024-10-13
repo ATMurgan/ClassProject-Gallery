4486a7e0dc6032c1f24e60c0142a9934fc99c85a
ATMurgan <156010971+ATMurgan@users.noreply.github.com>
2024-10-12 23:57:09 -0400

    model progress

--- ClassProject-Gallery/Models/Artwork.cs
+++ ClassProject-Gallery/Models/Artwork.cs
@@ -2,5 +2,23 @@
 {
     public class Artwork
     {
+        public int ArtworkId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // FK
+        public int ArtistId { get; set; }
+        public int CategoryId { get; set; }
+
+        // Parent ref
+        public Artist Artist { get; set; }
+
+        // Child ref
+        public List<OrderItem>? OrderItem { get; set; }
+        public List<Category> Categroy { get; set; }
+
+
     }
 }
--- ClassProject-Gallery/Models/User.cs
+++ ClassProject-Gallery/Models/User.cs
@@ -1,7 +1,28 @@
-﻿namespace ClassProject_Gallery.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassProject_Gallery.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        public string? ProfileImage { get; set; }
+
+        // Parent ref
+        public Order Order { get; set; }
+
+        // Child ref
+        public List<Artist>? Artist { get; set; }
 
     }
 }
